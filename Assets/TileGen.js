ed38fc125b1a16cdfed6ee67d7ccd39b8a21e8b7
Julian Poyourow <julianpoyo@gmail.com>
2015-05-27 21:34:05 -0700

    Very broken tile generator

--- Assets/TileGen.js
+++ Assets/TileGen.js
@@ -8,6 +8,9 @@ var ogTile : GameObject;
 //number of initialized prefabs
 var numMade;
 
+//number of active tileCreators
+var numActiveCreators;
+
 function Start()
 {
     //Get our first tile
@@ -18,6 +21,7 @@ function Start()
 
     //Create a tile every 2 seconds
     InvokeRepeating("tileCreate", 0, .5);
+    //tileCreate();
 }
 
 function Update()
@@ -28,8 +32,20 @@ function Update()
 //Create a tile
 function tileCreate()
 {
-  //Create a random direction to generate
-  var dir = Random.Range(0, 4);
+  var dir : double;
+  var validDirection = false;
+  while(!validDirection){
+  	//Create a random direction to generate
+    dir = Random.Range(0, 4);
+    
+    validDirection = true;
+    if((dir < 1 && (Physics2D.Raycast(transform.position, -Vector2.up).collider != null)) || 
+    	(dir >= 1 && dir < 2 && (Physics2D.Raycast(transform.position, Vector2.right).collider != null)) || 
+    	(dir >= 2 && dir < 3 && (Physics2D.Raycast(transform.position, Vector2.up).collider != null)) || 
+    	(dir >= 3 && dir <= 4 && (Physics2D.Raycast(transform.position, -Vector2.right).collider != null))){
+    	validDirection = false;
+    }
+  }
   //Our vector we are spawning
   var vector : Vector2;
 	vector = tile.transform.localPosition;
@@ -41,22 +57,22 @@ function tileCreate()
   if(dir < 1)
   {
       vector.y = vector.y - offset;
-      tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+      ogTile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
   else if(dir < 2)
   {
     vector.x = vector.x + offset;
-    tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+    ogTile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
   else if (dir < 3)
   {
     vector.y = vector.y + offset;
-    tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+    ogTile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
   else
   {
     vector.x = vector.x - offset;
-    tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+    ogTile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
 
 
@@ -71,18 +87,18 @@ function tileCreate()
   //now disable our trigger in the opposite direction
   if(dir < 1)
   {
-      ogTile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+    //tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
   else if(dir < 2)
   {
-    ogTile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+    //tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
   else if (dir < 3)
   {
-    ogTile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+    //tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
   else
   {
-    ogTile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+    //tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
   }
 }
