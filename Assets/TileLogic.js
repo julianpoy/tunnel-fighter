0bb65f04fb8b58274d08556e0888c457d72353bf
Julian Poyourow <julianpoyo@gmail.com>
2015-05-27 18:21:09 -0700

    Added get/set neighbors for TileLogic

--- Assets/TileLogic.js
+++ Assets/TileLogic.js
@@ -2,6 +2,10 @@
 
 var ChickenPos : Vector2;
 var tileShadow : GameObject;
+var neighborNorth : boolean = false;
+var neighborSouth : boolean = false;
+var neighborWest : boolean = false;
+var neighborEast : boolean = false;
 
 function Start () {
     ChickenPos = GameObject.Find("ChickenShit").transform.position;
@@ -14,4 +18,36 @@ function Update () {
 
 function OnTriggerEnter2D(other: Collider2D) {
 	tileShadow.SetActive(false);
+}
+
+function setNeighborNorth(){
+	neighborNorth = true;
+}
+
+function setNeighborSouth(){
+	neighborSouth = true;
+}
+
+function setNeighborWest(){
+	neighborWest = true;
+}
+
+function setNeighborEast(){
+	neighborEast = true;
+}
+
+function hasNeighborNorth(){
+	return neighborNorth;
+}
+
+function hasNeighborSouth(){
+	return neighborSouth;
+}
+
+function hasNeighborWest(){
+	return neighborWest;
+}
+
+function hasNeighborEast(){
+	return neighborEast;
 }
