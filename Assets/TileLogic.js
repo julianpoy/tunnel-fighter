1d637585239208e7cee319afdbcad67a18859cd6
Aaron Turner <torch2424@hotmail.com>
2015-05-27 19:28:55 -0700

    It's the little Details that matter ;)

--- Assets/TileLogic.js
+++ Assets/TileLogic.js
@@ -10,15 +10,36 @@ var neighborEast : boolean = false;
 function Start () 
 {
     Chicken = GameObject.Find("ChickenShit");
+    //Get our tile shadow and make sure it is correct
     tileShadow = gameObject.transform.Find("Shadow").gameObject;
+    tileShadow.SetActive(true);
+    tileShadow.GetComponent(SpriteRenderer).color.a = 255;
 }
 
 function OnTriggerEnter2D(other: Collider2D) 
 {
 	if(other.gameObject.Equals(Chicken))
 	{
-		tileShadow.SetActive(false);
+		StartCoroutine("SmoothEnter");
 	}
+}
+
+//Smooth Enter function to reveal our tile
+function SmoothEnter()
+{
+	//Get our tile shadow color
+	var shadow : Color32 = tileShadow.GetComponent(SpriteRenderer).color;
+	
+	//while our alpha value is full, decrease it until the shadow is gone
+	while(shadow.a > 0)
+	{
+		shadow.a = shadow.a / 1.065f;
+		tileShadow.GetComponent(SpriteRenderer).color = shadow;
+		yield WaitForSeconds(.01f);
+	}
+	
+	//tile is now smoothly exited, exit it 
+	tileShadow.SetActive(false);
 }
 
 function setNeighborNorth(){
