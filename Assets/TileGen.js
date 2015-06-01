e00cc35e61464ee94bfeb46b87ba5aa8255d656a
Julian Poyourow <julianpoyo@gmail.com>
2015-06-01 14:27:02 -0700

    Code cleanup

--- Assets/TileGen.js
+++ Assets/TileGen.js
@@ -8,14 +8,11 @@ var ogTile : GameObject;
 //The main player object
 var player : GameObject;
 
-//Keep track of a mapGen crash (Every 2nd map generation ends with a mapGen crash)
-var crash : int;
+//Keep track of if generation backs itself into a corner
+var genStopped : boolean;
 
 //number of initialized prefabs
 var numMade : int;
-
-//number of active tileCreators
-var numActiveCreators;
 
 function Start()
 {
@@ -29,7 +26,7 @@ function Start()
     tile = ogTile;
 
     //Create a tile every 2 seconds
-    crash = 0;
+    genStopped = false;
     tileCreate();
 }
 
@@ -41,138 +38,149 @@ function Update()
 //Create a tile
 function tileCreate()
 {
-  var dir : double;
-  var blockSouth: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierSouth").transform.position.x, tile.transform.Find("BarrierSouth").transform.position.y - 1), -Vector2.up, 2.5);
-  var blockEast: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierEast").transform.position.x + 1, tile.transform.Find("BarrierEast").transform.position.y), Vector2.right, 2.5);
-  var blockNorth: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierNorth").transform.position.x, tile.transform.Find("BarrierNorth").transform.position.y + 1), Vector2.up, 2.5);
-  var blockWest: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierWest").transform.position.x - 1, tile.transform.Find("BarrierWest").transform.position.y), -Vector2.right, 2.5);
-  var validDirection = false;
-  var validityCrash : int;
-  while(!validDirection){
-  	validityCrash = validityCrash + 1;
-  	//Create a random direction to generate
-    dir = Random.Range(0, 4);
-    //dir = 0;
-    validDirection = true;
-	//Debug.Log(dir);
-	
-	
-    //Debug.Log(hitWest.transform.name);
-    if((dir < 1 && (blockSouth)) || 
-    	(dir >= 1 && dir < 2 && (blockEast)) || 
-    	(dir >= 2 && dir < 3 && (blockNorth)) || 
-    	(dir >= 3 && dir <= 4 && (blockWest))){
-    	validDirection = false;
-    	
-    		//Debug.Log("fuck yo couch");
-    	
-    }
-    if(validityCrash > 200){
-    	Debug.Log("Validity Crash!!!");
-    	crash = 1;
-    	break;
-    }
-  }
-  if(crash == 0){
-	  //Our vector we are spawning
-	  var vector : Vector2;
+	//Declare direction container
+	var dir : double;
+
+	//Shoot RayCasts in each direction to detect ajacent tiles
+	var blockSouth: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierSouth").transform.position.x, tile.transform.Find("BarrierSouth").transform.position.y - 1), -Vector2.up, 2.5);
+	var blockEast: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierEast").transform.position.x + 1, tile.transform.Find("BarrierEast").transform.position.y), Vector2.right, 2.5);
+	var blockNorth: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierNorth").transform.position.x, tile.transform.Find("BarrierNorth").transform.position.y + 1), Vector2.up, 2.5);
+	var blockWest: RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.Find("BarrierWest").transform.position.x - 1, tile.transform.Find("BarrierWest").transform.position.y), -Vector2.right, 2.5);
+
+	//Keep picking until a valid direction is found
+	var validDirection = false;
+	//Quit if it runs too many times (signifies that gen has entered a corner)
+	var directionCrash : int;
+	while(!validDirection){
+		//Increment the crash catch counter
+		directionCrash = directionCrash + 1;
+
+		//Choose a random direction
+		dir = Random.Range(0, 4);'
+
+		//Assume direction is valid
+		validDirection = true;
+
+		//Check if chosen direction is blocked
+		if((dir < 1 && (blockSouth)) || 
+			(dir >= 1 && dir < 2 && (blockEast)) || 
+			(dir >= 2 && dir < 3 && (blockNorth)) || 
+			(dir >= 3 && dir <= 4 && (blockWest))){
+			//If so, that direction was not valid
+			validDirection = false;
+		}
+
+		//Check if the direction picker has been running too many times
+		if(directionCrash > 200){
+		Debug.Log("Generator Stopped Early");
+		genStopped = true;
+		break;
+		}
+	}
+
+	//If generation is a go, run the main gen code
+	if(genStopped == false){
+		//Our vector we will be spawning
+		var vector : Vector2;
 		vector = tile.transform.localPosition;
-		
+
 		//Our offset from the previous tile
 		var offset = 2.9f;
 
-	  //enable our is trigger for the old tile and vector 2 for the new tile
-	  if(dir < 1)
-	  {
-	      vector.y = vector.y - offset;
-	  }
-	  else if(dir < 2)
-	  {
-	    vector.x = vector.x + offset;
-	  }
-	  else if (dir < 3)
-	  {
-	    vector.y = vector.y + offset;
-	  }
-	  else
-	  {
-	    vector.x = vector.x - offset;
-	  }
+		//Generate vectors for the corrosponding direction
+		if(dir < 1)
+		{
+		  vector.y = vector.y - offset;
+		}
+		else if(dir < 2)
+		{
+			vector.x = vector.x + offset;
+		}
+		else if (dir < 3)
+		{
+			vector.y = vector.y + offset;
+		}
+		else
+		{
+			vector.x = vector.x - offset;
+		}
 
 
-	  //Set the shadow to true on the tile
-	  tile.transform.Find("Shadow").gameObject.SetActive(true);
+		//Set the shadow to true on the tile
+		tile.transform.Find("Shadow").gameObject.SetActive(true);
 
-	  //save our newest tile, then spawn our new current tile
-	  ogTile = tile;
-	  tile = Instantiate(tile,vector,Quaternion.identity);
-	  
-	  //Disable BoxCollider on player
-	  player.GetComponent(BoxCollider2D).enabled = false;
-	  tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).enabled = false;
-	  tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).enabled = false;
-	  tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).enabled = false;
-	  tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).enabled = false;
-	  
-	  var hitSouth : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), -Vector2.up, 3);
-	  var hitEast : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), Vector2.right, 3);
-	  var hitNorth : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), Vector2.up, 3);
-	  var hitWest : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), -Vector2.right, 3);
+		//Save the old tile for future reference, set tile to mean the newly generated tile
+		ogTile = tile;
+		tile = Instantiate(tile,vector,Quaternion.identity);
 
-	  //now disable our trigger in the opposite direction
-	  if(hitNorth)
-	  {
-	    Debug.Log(numMade + " North " + hitNorth.collider);
-	    tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	    Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitNorth.collider.transform.position, Color.white, 5f, false);
-	    //var tileNorth : GameObject = hitNorth.collider.gameObject;
-	    hitNorth.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	  } else {
-	  	tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
-	  }
-	  if(hitWest)
-	  {
-	   	Debug.Log(numMade + " West " + hitWest.collider);
-	   	tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	   	Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitWest.collider.transform.position, Color.white, 5f, false);
-	    //var tileWest : GameObject = hitWest.collider.gameObject;
-	    hitWest.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	  } else {
-	  	tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
-	  }
-	  if(hitEast)
-	  {
-	    Debug.Log(numMade + " East " + hitEast.collider);
-	    tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	    Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitEast.collider.transform.position, Color.white, 5f, false);
-	    //var tileEast : GameObject = hitEast.collider.gameObject;
-	    hitEast.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	  } else {
-	  	tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
-	  }
-	  if(hitSouth)
-	  {
-	    Debug.Log(numMade + " South " + hitSouth.collider);
-	    tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	    Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitSouth.collider.transform.position, Color.white, 5f, false);
-	    //var tileSouth : GameObject = hitSouth.collider.gameObject;
-	    hitSouth.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
-	  } else {
-	  	tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
-	  }
-	  //Re-enable box collider for player
-	  player.GetComponent(BoxCollider2D).enabled = true;
-	  tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).enabled = true;
-	  tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).enabled = true;
-	  tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).enabled = true;
-	  tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).enabled = true;
-	  
-	  //Increment number of tiles made
-	  numMade = numMade + 1;
-	  
-	  //Call the function again
-	  if(numMade < 100 && crash == 0){
-	  	tileCreate();
-	  }
+		//Disable BoxCollider on player and new tile
+		player.GetComponent(BoxCollider2D).enabled = false;
+		tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).enabled = false;
+		tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).enabled = false;
+		tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).enabled = false;
+		tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).enabled = false;
+
+		//Check for adjacent tiles
+		var hitSouth : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), -Vector2.up, 3);
+		var hitEast : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), Vector2.right, 3);
+		var hitNorth : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), Vector2.up, 3);
+		var hitWest : RaycastHit2D = Physics2D.Raycast(new Vector2(tile.transform.position.x, tile.transform.position.y), -Vector2.right, 3);
+
+		//For each direction, if there is an adjacent tile, set the boxcollider2d istrigger of both the current tile and the adjacent tile
+		if(hitNorth)
+		{
+			//Debug.Log(numMade + " North " + hitNorth.collider);
+			tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+			//Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitNorth.collider.transform.position, Color.white, 5f, false);
+			//var tileNorth : GameObject = hitNorth.collider.gameObject;
+			hitNorth.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+		} else {
+			tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
+		}
+		if(hitWest)
+			{
+			//Debug.Log(numMade + " West " + hitWest.collider);
+			tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+			//Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitWest.collider.transform.position, Color.white, 5f, false);
+			//var tileWest : GameObject = hitWest.collider.gameObject;
+			hitWest.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+		} else {
+			tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
+		}
+		if(hitEast)
+		{
+			//Debug.Log(numMade + " East " + hitEast.collider);
+			tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+			//Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitEast.collider.transform.position, Color.white, 5f, false);
+			//var tileEast : GameObject = hitEast.collider.gameObject;
+			hitEast.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+		} else {
+			tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
+		}
+		if(hitSouth)
+		{
+			//Debug.Log(numMade + " South " + hitSouth.collider);
+			tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+			//Debug.DrawLine(new Vector2(tile.transform.position.x, tile.transform.position.y), hitSouth.collider.transform.position, Color.white, 5f, false);
+			//var tileSouth : GameObject = hitSouth.collider.gameObject;
+			hitSouth.collider.gameObject.GetComponent(BoxCollider2D).isTrigger = true;
+		} else {
+			tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).isTrigger = false;
+		}
+		
+		//Re-enable box collider for player and new tile
+		player.GetComponent(BoxCollider2D).enabled = true;
+		tile.transform.Find("BarrierNorth").gameObject.GetComponent(BoxCollider2D).enabled = true;
+		tile.transform.Find("BarrierWest").gameObject.GetComponent(BoxCollider2D).enabled = true;
+		tile.transform.Find("BarrierEast").gameObject.GetComponent(BoxCollider2D).enabled = true;
+		tile.transform.Find("BarrierSouth").gameObject.GetComponent(BoxCollider2D).enabled = true;
+
+		//Increment number of tiles made
+		numMade = numMade + 1;
+
+		//Call the function again
+		if(numMade < 100 && genStopped == false){
+			tileCreate();
+		}
 	}
 }
